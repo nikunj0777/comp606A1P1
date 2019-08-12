1e2ac8cad7db2ad79c9bb18c87f5a7fa269e841f
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-08-12 21:02:37 +1200

    wrongDetails.php
    This file contains an error message of a user's wrong detail.

---
+++ wrongDetails.php
@@ -0,0 +1,22 @@
+<?php
+include_once('connect.php');
+
+if(!isset($_SESSION['username'])){
+    header("Location:login.php");
+}
+?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Style.css">
+<title>WrongDetails</title>
+<body>
+</body>
+
+<h1>Please enter valid details...!!!</h2>
+<a class= logout href="logout.php">logout</a>
+
+
+
+</head>
+</html>
