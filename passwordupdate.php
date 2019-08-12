d854ccde27be568dbc32fdd44d6ffe603ee6310e
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-08-12 20:59:44 +1200

    Update passwordupdate.php

--- passwordupdate.php
+++ passwordupdate.php
@@ -1,19 +1,20 @@
 
 <?php
+//connection execute
 include_once('connect.php');
 
 if(!isset($_SESSION['username'])){
     header("Location:login.php");
 }
 ?>
-
-include 
+ 
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="Style.css">
 <title>passwordupdate</title>
 <body>
 </body>
+    //back to login page
 <a href="login.php"> --Login-- </a>
 <h1>password successfuly update...!!!</h2>
 
