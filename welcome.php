500e3b5ae30e5702e6a21b99525d066d1ab32bad
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-08-12 20:46:50 +1200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,20 +1,20 @@
 <?php
+//connection execute
 include_once('connect.php');
 
 if(!isset($_SESSION['username'])){
     header("Location:login.php");
 }
 ?>
-
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="Style.css">
-<title>Login FORM</title>
-<body>
-<div class="login-page">
-<h1>Welcome <?php echo $_SESSION['username']; ?> !</h1>
-  <a href="abc.php"> Reset Password!! </a> <br>
-  <a href="logout.php"> Logout </a>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+          <title>Login FORM</title>
+    <body>
+        <div class="login-page">
+        <h1>Welcome <?php echo $_SESSION['username']; ?> !</h1>
+           <a href="resetpassword.php"> Reset Password!! </a> <br>
+           <a href="logout.php"> Logout </a>
 </div>
 
 </body>
