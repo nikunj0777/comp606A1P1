8d473897c93d203d371da9a02d7cfc0c03606f29
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-08-12 20:43:07 +1200

    welcome.php
    This is the greeting page for the user. Moreover, it has two functionality first is reset password and second is logout.

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<?php
+include_once('connect.php');
+
+if(!isset($_SESSION['username'])){
+    header("Location:login.php");
+}
+?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Style.css">
+<title>Login FORM</title>
+<body>
+<div class="login-page">
+<h1>Welcome <?php echo $_SESSION['username']; ?> !</h1>
+  <a href="abc.php"> Reset Password!! </a> <br>
+  <a href="logout.php"> Logout </a>
+</div>
+
+</body>
+</head>
+</html>
